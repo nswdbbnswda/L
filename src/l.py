383b269a0804eb8d0b980eeb1782fefc1de1b528
nswdbbnswda <nswdbbnswda@163.com>
2018-12-31 14:09:25 +0800

    init

---
+++ src/l.py
@@ -0,0 +1,24 @@
+#coding:utf8
+
+import wave
+import numpy as np
+from math import cos, sin, sqrt, pi
+
+
+
+
+class L(object):
+   
+    def __init__(self):
+        print("你好，我是L!")
+
+
+    
+
+
+
+
+
+
+if __name__ =="__main__":
+   l = L() 
